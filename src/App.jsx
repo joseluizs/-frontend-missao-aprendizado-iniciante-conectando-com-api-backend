ce720e09e75acaf02b85cd4c71e0147dbfbb1b11
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-09-28 18:43:56 +0000

    Arquivos iniciais do Projeto

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css'
+import Card from './components/Card/Card'
+
+function App() {
+  const java = {
+    nome: 'Java',
+    imagem: 'https://www.salvatore.academy/devmon/1_java.png',
+    evoluiPara: 'Kotlin'
+  }
+
+  const kotlin = {
+    nome: 'Kotlin',
+    imagem: 'https://www.salvatore.academy/devmon/2_kotlin.png',
+    evoluiPara: 'Android'
+  }
+
+  const android = {
+    nome: 'Android',
+    imagem: 'https://www.salvatore.academy/devmon/3_android.png',
+    evoluiPara: ''
+  }
+
+  const c = {
+    nome: 'C',
+    imagem: 'https://www.salvatore.academy/devmon/4_c.png',
+    evoluiPara: 'C++'
+  }
+
+  const cplusplus = {
+    nome: 'C++',
+    imagem: 'https://www.salvatore.academy/devmon/5_cpp.png',
+    evoluiPara: 'C#'
+  }
+
+
+  const devmons = [java, kotlin, android, c, cplusplus]
+
+  return (
+    <>
+      <div className='cards'>
+        {devmons.map(function (devmon) {
+          return <Card key={devmon.nome} item={devmon} />
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
