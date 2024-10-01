d603e759bd0d9d9f07c990780ea80128d5fee442
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-10-01 14:08:06 +0000

    Tratamento de erros com `react-toastify`

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
+import { toast, ToastContainer } from 'react-toast'
 
 function App() {
 
@@ -10,13 +11,20 @@ function App() {
 
     const apiUrl = 'https://backend-integrar-com-frontend-c290.onrender.com/personagem'
 
-    const response = await fetch(apiUrl)
 
-    const data = await response.json()
+    const response = await fetch(apiUrl).catch(function (error){
+      console.error('Erro ao chamar endpoint /personagem', error)
+      toast.error('Erro ao garregar lista de DevMons.')
+    })
 
-    setDevmons(data)
+    if (response.ok) {
+      const data = await response.json()
+      setDevmons(data)
+    } else {
+      toast.error('Erro ao garregar lista de DevMons.')
+    }
 
-    devmons.push(...data)
+    console.error(error)
 
   }
 
@@ -31,6 +39,7 @@ function App() {
           return <Card key={devmon.nome} item={devmon} />
         })}
       </div>
+      <ToastContainer />
     </>
   )
 }
